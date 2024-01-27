388a02f4cd1dc8b9738e6e072a72c3144ca740a6
Surdeep911 <voxton95@gmail.com>
2024-01-27 12:17:25 +0000

    modified api test file

--- test/restAPI.js
+++ test/restAPI.js
@@ -1,34 +1,31 @@
 
-// require('dotenv').config()
-const { NEXT_PUBLIC_XRAPIDAPIKEY } = process.env
+require('dotenv').config()
 
+let hostname = 'worldwide-restaurants.p.rapidapi.com'
+let header = {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_XRAPIDAPIKEY,
+    'X-RapidAPI-Host': hostname
+}
+let search = {
+    location_id: '15333491',
+    language: 'en_US',
+    currency: 'USD',
+    offset: '0'
+}
 
 const callApi = async () => {
-    const url = 'https://worldwide-restaurants.p.rapidapi.com/reviews';
+    const url = `https://${hostname}/reviews`
     const options = {
         method: 'POST',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-RapidAPI-Key': 'e3ff25b962msh585e4f27493fb2dp1f6e48jsn069d06c3cfe4',
-            'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-        },
-        body: new URLSearchParams({
-            location_id: '15333491',
-            language: 'en_US',
-            currency: 'USD',
-            offset: '0'
-        })
+        headers: header,
+        body: new URLSearchParams(search)
     };
     try {
         const res = await fetch(url, options);
         const data = await res.json();
         console.log(data)
-        // var size = Object.keys(data).length;
-        // let response_fromtest = await test(data.results.data)
-        // const result = await classify(data.results.data[3].text)
-    } catch (err) {
-        console.log(err);
-    }
+    } catch (err) {console.log(err)}
 }
 
 callApi()
